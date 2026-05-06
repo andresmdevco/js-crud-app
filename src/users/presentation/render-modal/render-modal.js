c6e2ec84113e5c289bc3e610cb8e8034b066c905
Andrés Muñoz <andres2078@gmail.com>
2026-05-05 23:56:41 -0500

    feat: load user data into modal for editing

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,13 +1,22 @@
 import modalHtml from './render-modal.html?raw';
 import './render-modal.css';
-import { createNodeImportMeta } from 'vite/module-runner';
+import { User } from '../../models/user';
+import { getUserById } from '../../use-cases/get-user-by-id';
 
 let modal, form;
+let loadedUser = {};
 
-
-// TODO: cargar usuario por id
-export const showModal = () => {
+/**
+ * 
+ * @param {String|Number} id 
+ */
+export const showModal = async(id) => {
     modal?.classList.remove('hide-modal')
+    loadedUser = {};
+
+    if (!id) return;
+    const user = await getUserById(id);
+    setFormValues(user);
 }
 
 
@@ -15,6 +24,19 @@ export const hideModal = () => {
     modal?.classList.add('hide-modal')
     form?.reset();
 }
+
+/**
+ * 
+ * @param {User} user 
+ */
+const setFormValues = (user) => {
+    form.querySelector('[name="firstName"]').value = user.firstName;
+    form.querySelector('[name="lastName"]').value = user.lastName;
+    form.querySelector('[name="balance"]').value = user.balance;
+    form.querySelector('[name="isActive"]').checked = user.isActive;
+    loadedUser = user;
+}
+
 
 /**
  * 
@@ -40,7 +62,7 @@ export const renderModal = (element, callback) => {
         event.preventDefault();
 
         const formData = new FormData(form);
-        const userLike = {};
+        const userLike = {...loadedUser};
 
         for (const [key, value] of formData) {
             if (key === 'balance') {
